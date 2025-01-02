d67c92608bc9863bcab95faa2966d0ea6e362327
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-01-02 23:55:54 +0100

    Feature: implement neigbour solutions

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from models.City import generateCities, processDistanceMatrix
 from vue.visualization import path_visualization
-from models.Plan import process_tour_total_distance
+from models.Plan import process_tour_total_distance, generate_derivated_plan
 
 # launch the main function
 cities = generateCities(8)
 distances_matrix = processDistanceMatrix(cities, 0)
 
-print("The total distance of the tour is: ", process_tour_total_distance(cities))
-path_visualization(cities)
+plan = cities
+derivated_plan = generate_derivated_plan(plan)
+
+print("The total distance of the tour is: ", process_tour_total_distance(plan))
+path_visualization(plan)
+
+print("The total distance of the derivated tour is: ", process_tour_total_distance(derivated_plan))
+path_visualization(derivated_plan)
--- models/Plan.py
+++ models/Plan.py
@@ -4,22 +4,44 @@ Gilles NGASSAM
 """
 
 # librairies importation
+import random as rd
+from copy import deepcopy
 from utils.Point import Point
 
 # Calculate the total distance of the tour
-def process_tour_total_distance(cities: list[Point]) -> float:
+def process_tour_total_distance(plan: list[Point]) -> float:
     """
     Calculate the total distance of the tour
 
     Args:
-        cities (list[Point]): a group of cities in the order to visit
+        plan (list[Point]): a group of cities in the order to visit
 
     Returns:
         float: the total distance of the tour
     """
     total_distance: float = 0
 
-    for i in range(cities.__len__()):
-        total_distance += cities[i].calculateDistanceTo(cities[(i+1)%cities.__len__()])
+    for i in range(plan.__len__()):
+        total_distance += plan[i].calculateDistanceTo(plan[(i+1)%plan.__len__()])
 
     return total_distance
+
+# Generate a new plan from the previous one by swapping two cities
+def generate_derivated_plan(plan: list[Point]) -> list[Point]:
+    """
+    Generate a new plan from the previous one by swapping two cities
+
+    Args:
+        plan (list[Point]): the previous plan (a group of cities in the order to visit)
+
+    Returns:
+        list[Point]: the new plan derived from the given one
+    """
+    # Copy the previous plan
+    new_plan: list[Point] = deepcopy(plan)
+
+    # Swap the two cities
+    exchange_array: list[int] = rd.sample(range(0, plan.__len__()), 2)
+    new_plan[exchange_array[0]], new_plan[exchange_array[1]] = new_plan[exchange_array[1]], new_plan[exchange_array[0]]
+
+    return new_plan
